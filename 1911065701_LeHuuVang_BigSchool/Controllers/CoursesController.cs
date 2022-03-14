d3b7219e9483ded95b038745243bd378fe8899df
asus <asus@DESKTOP-DV6JQ3L>
2022-03-14 16:29:45 +0700

    Add project files.

---
+++ 1911065701_LeHuuVang_BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,33 @@
+﻿using _1911065701_LeHuuVang_BigSchool.Models;
+using _1911065701_LeHuuVang_BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1911065701_LeHuuVang_BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public List<Category> Categories { get; set; }
+
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel();
+            {
+                Categories = _dbContext.Categories.ToList();
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ 1911065701_LeHuuVang_BigSchool/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Build.Framework.XamlTypes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _1911065701_LeHuuVang_BigSchool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        [Required]
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime DataTime { get; set; }
+        public Category Categoryy { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }    
+    }
+}
